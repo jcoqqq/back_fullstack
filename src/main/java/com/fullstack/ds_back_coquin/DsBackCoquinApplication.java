f7fa3e4bbed41a2f98961bde628f808533b8ab46
jcoquin <julien.coquin@equasens.com>
2023-12-08 09:44:25 +0100

    creation projet back

---
+++ src/main/java/com/fullstack/ds_back_coquin/DsBackCoquinApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.ds_back_coquin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsBackCoquinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsBackCoquinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/ds_back_coquin/DsBackCoquinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.ds_back_coquin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsBackCoquinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
